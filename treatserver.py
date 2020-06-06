f0d046b92dba054b32588eeb0d7b996a6127ac36
Evan Brandt <evanmbrandt@gmail.com>
2020-06-06 11:05:14 -0700

    added readme

--- treatserver.py
+++ treatserver.py
@@ -1,29 +1,23 @@
 import RPi.GPIO as GPIO
 import os
-from time
+import time
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 host_name = '192.168.1.106'  # Change this to your Raspberry Pi IP address
 host_port = 8000
-servo_control_pin = 18
+motor_control_pin = 18
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-def cycle_servo():
+#setup pin to pull down
+GPIO.setup(motor_control_pin, GPIO.OUT)
+GPIO.output(motor_control_pin, GPIO.LOW)
+
+def cycle_motor():
     # setup servo control on pin 
-    GPIO.setup(servo_control_pin,GPIO.OUT)
-    servo1 = GPIO.PWM(servo_control_pin,50) # Note 18 is pin, 50 = 50Hz pulse
-    servo1.start(0) #start PWM running, but with value of 0 (pulse off)
-    time.sleep(1)
-    servo1.ChangeDutyCycle(12)
-    time.sleep(1)
-    servo1.ChangeDutyCycle(2)
-    time.sleep(1)
-    servo1.ChangeDutyCycle(0)
-    # Wait a couple of seconds
-    time.sleep(1)
-    servo1.stop()
-    GPIO.cleanup()
+    GPIO.output(motor_control_pin, GPIO.HIGH)
+    time.sleep(4)
+    GPIO.output(motor_control_pin, GPIO.LOW)
     print ("Cycle done")
 
 class MyServer(BaseHTTPRequestHandler):
@@ -45,30 +39,43 @@ class MyServer(BaseHTTPRequestHandler):
         """
         html = '''
            <html>
-           <body style="width:960px; margin: 20px auto;">
-           <h1>Welcome to my Raspberry Pi</h1>
-           <p>Current GPU temperature is {}</p>
-           <p>Turn LED: <a href="/on">On</a> <a href="/off">Off</a></p>
-           <div id="led-status"></div>
-           <script>
-               document.getElementById("led-status").innerHTML="{}";
-           </script>
-           </body>
-           </html>
+            <head>
+            <meta http-equiv="content-type" content="text/html; charset=windows-1252">
+            <style>
+            a.button {
+                -webkit-appearance: button;
+                -moz-appearance: button;
+                appearance: button;
+                text-decoration: none;
+                color: initial;
+                height: 300px;
+                display: inline-block;
+                width: 50%;
+                text-align: center;
+                line-height: 300px;
+                font-size: 3em;
+            }
+
+            body{
+                text-align: center;
+            }
+
+            </style>
+            </head>
+            <body>
+            <h1>Welcome to Treats!</h1>
+            <p><a class="button" href="dispense">Dispense Treat</a></p>
+            </body>
+            </html>
         '''
         temp = os.popen("/opt/vc/bin/vcgencmd measure_temp").read()
         self.do_HEAD()
-        status = ''
+
         if self.path=='/':
             GPIO.setmode(GPIO.BCM)
-        elif self.path=='/on':
-            #GPIO.output(18, GPIO.HIGH)
-            cycle_servo()
-            status='LED is On'
-        elif self.path=='/off':
-            #GPIO.output(18, GPIO.LOW)
-            status='LED is Off'
-        self.wfile.write(html.format(temp[5:], status).encode("utf-8"))
+        elif self.path=='/dispense':
+            cycle_motor()
+        self.wfile.write(html.encode("utf-8"))
 
 
 if __name__ == '__main__':
